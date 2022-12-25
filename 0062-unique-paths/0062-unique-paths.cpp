e7b1bb52ce17471ae39dc54312f02e04249831b8
Abhi Mummaneni <abmummaneni@gmail.com>
2022-12-25 18:25:38 -0500

    Time: 0 ms (100.00%), Space: 6.1 MB (70.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,12 @@
+
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-    if (m == 1 || n == 1){
-        return 1;
-    }
-    int** arr = new int*[m + 1]{};
-    for(int i = 0; i < m + 1; ++i){arr[i] = new int[n + 1]{};}
-    arr[1][1] = 1;
-    arr[2][1] = 1;
-    arr[1][2] = 1;
-    for(int i = 1; i <= m; ++i){
-        for (int j = 1; j <= n; ++j) {
-            if (i == 1 || j == 1){
-                arr[i][j] = 1;
-            }
-            else {
-                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
-            }
+        double ans = 1;
+        for(double i = 1; i <= m - 1; i++){
+            ans *= (((double) n - 1) / i + 1);
         }
+        ans += 0.5; // round
+        return (int) ans;
     }
-    return arr[m][n];
-}
 };
